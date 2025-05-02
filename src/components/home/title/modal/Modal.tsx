1ccef1e801e424b81a2b067a84530acb2dee91e2
inevadim <kvadrocheyk@gmail.com>
2025-05-02 17:19:33 +0300

    bug modal window visible

--- src/components/home/title/modal/Modal.tsx
+++ src/components/home/title/modal/Modal.tsx
@@ -1,14 +1,27 @@
 import styles from './Modal.module.scss';
 
+
 type Props={
     active:boolean,
-    setActiveModal:boolean,
+    // setActiveModal:boolean,
+    setActiveModal:any,
 }
+
+
+
+
 
 function Modal({active, setActiveModal}:Props) {
     return (
         <div className={active ? styles.modal : styles.unModal} onClick={()=>setActiveModal(false)}>
-            <div className={styles.content} onClick={(e)=>e.stopPropagation()}></div>
+            <div className={styles.content} onClick={(e)=>e.stopPropagation()}>
+                <form>
+                    <input placeholder='name'/>
+                    <input type='tel' placeholder='number'/>
+                    <input type='email' placeholder='email'/>
+                    <button type="button">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
