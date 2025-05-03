e59ff25afa0a16d0f054e5ee1fa36d1c911f90f0
inevadim <kvadrocheyk@gmail.com>
2025-05-03 12:32:15 +0300

    refactoring Modal title and add glasmorphizm style modal window

--- src/components/home/title/modal/Modal.tsx
+++ src/components/home/title/modal/Modal.tsx
@@ -1,21 +1,19 @@
 import styles from './Modal.module.scss';
 
-
-type Props={
-    active:boolean,
-    setActiveModal:any,
+type Props = {
+    active: boolean,
+    setActiveModal: any,
 }
 
-function Modal({active, setActiveModal}:Props) {
+function Modal({ active, setActiveModal }: Props) {
     return (
-        <div className={active ? styles.modal : styles.unModal} onClick={()=>setActiveModal(false)}>
-            <div className={styles.content} onClick={(e)=>e.stopPropagation()}>
-            <span>Contact with me</span>
+        <div className={active ? styles.modal : styles.unModal} onClick={() => setActiveModal(false)}>
+            <div className={styles.content} onClick={(e) => e.stopPropagation()}>
+                <span>Contact with me</span>
                 <form className={styles.formModal}>
-                    
-                    <input placeholder='name'/>
-                    <input type='tel' placeholder='phone'/>
-                    <input type='email' placeholder='email'/>
+                    <input placeholder='name' />
+                    <input type='tel' placeholder='phone' />
+                    <input type='email' placeholder='email' />
                     <textarea placeholder="Enter message..." className={styles.myTextarea} />
                     <button className={styles.buttonModal} type="button">Submit</button>
                 </form>
