1dcfcdadf08c9c80eafadd5d1082c6b709654a2b
inevadim <kvadrocheyk@gmail.com>
2025-05-03 12:16:54 +0300

    refactoring Logo

--- src/components/home/header/logo/Logo.tsx
+++ src/components/home/header/logo/Logo.tsx
@@ -1,19 +1,13 @@
-// import styled from "styled-components";
-import styles from './Logo.module.css';
+import styles from './Logo.module.scss';
 import logo from "../../../../assets/img/svg/logo.svg";
+
 function Logo() {
     return (
         <div className={styles.logo}>
-            <img src={logo} alt="logo"/>
-            <span className={styles.logoTitle}>IneVadim</span>
+            <img src={logo} alt="logo" />
+            <span>IneVadim</span>
         </div>
     )
 }
 
 export default Logo
-
-// const Title1 = styled.h1`
-//     font-size: 1.5em;
-//     text-align: center;
-//     color: #e91e63;
-// `;
