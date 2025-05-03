d84581f5596476b416a92817022b1cdb48531a28
inevadim <kvadrocheyk@gmail.com>
2025-05-03 19:12:58 +0300

    structure sticky nav

--- src/components/home/header/menu/Menu.tsx
+++ src/components/home/header/menu/Menu.tsx
@@ -2,14 +2,14 @@ import styles from './Menu.module.scss';
 
 function Menu() {
     return (
-        <div className={styles.menu}>
+        <nav className={styles.menu}>
             <ul>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#aboutMe">About me</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-        </div>
+        </nav>
     )
 }
 
