91d300f3290656b3936578c23f4c2bf5f10249d9
inevadim <kvadrocheyk@gmail.com>
2025-05-03 12:21:58 +0300

    refactoring Men

--- src/components/home/men/Men.tsx
+++ src/components/home/men/Men.tsx
@@ -1,18 +1,17 @@
 import Circle from './circle/Circle';
-import styles from './Men.module.css';
+import styles from './Men.module.scss';
 import circle from "../../../assets/img/svg/blueCircle.svg";
 import menImg from "../../../assets/img/meMain.png";
 
 function Men() {
     return (
         <div className={styles.men}>
-            
-            <img className={styles.blueCircle} src={circle} alt="circle" width={600}/>
-            <span className={styles.ps}><Circle title="PS"/></span>
-            <span className={styles.ux}><Circle title="UX"/></span>
-            <span className={styles.code}><Circle title="</"/></span>
-            <span className={styles.ui}><Circle title="UI"/></span>
-            <img className={styles.menImg} src={menImg} alt="men" width={350}/>
+            <img className={styles.blueCircle} src={circle} alt="circle" width={600} />
+            <span className={styles.ps}><Circle title="PS" /></span>
+            <span className={styles.ux}><Circle title="UX" /></span>
+            <span className={styles.code}><Circle title="</" /></span>
+            <span className={styles.ui}><Circle title="UI" /></span>
+            <img className={styles.menImg} src={menImg} alt="men" width={350} />
         </div>
     )
 }
