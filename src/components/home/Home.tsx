19926e82628bfc14e8371b0afaa09b627ec07214
inevadim <kvadrocheyk@gmail.com>
2025-05-04 18:55:10 +0300

    adaptive 320px

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,4 +1,3 @@
-// import Header from "../header/Header";
 import styles from './Home.module.scss';
 import Men from "./men/Men";
 import Title from "./title/Title";
@@ -6,13 +5,11 @@ import Title from "./title/Title";
 function Home() {
   return (
     <div className={styles.wrap} id="home">
-      <div className={styles.home}>
-        {/* <Header /> */}
-        <div className={styles.wrapTitleMen}>
-          <Title />
-          <Men />
-        </div>
+      <div className={styles.wrapTitleMen}>
+        <Title />
+        <Men />
       </div>
+
     </div>
   )
 }
